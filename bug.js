dd4188efe1d1fba44b2f22ef03ad5b950102a28d
BugHunter-X <askzenai@gmail.com>
2025-01-30 14:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  response.end(JSON.stringify({ message: 'Hello, World!' }));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
